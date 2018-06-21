293138f227638085f24c0cf9959fc55505359548
Bryan-C-W <jabrway96@gmail.com>
2018-06-21 18:41:17 -0500

    Load Wheel hidder
    Added means to hide loading wheel

--- fetch.js
+++ fetch.js
@@ -1,7 +1,10 @@
 fetch('http://www.sharproot.com/wp-json/wp/v2/posts?categories=28') 
-.then(function() {
-
+.then(function(vendors) {
+    var sipnner = document.getElementById("spinner");
+    spinner.style.display="none";
+    acceptVendors(vendors);
 })
 .catch(function() {
-
+console.log("Husten, we have a problem.")
 });
+
