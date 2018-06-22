48eac8cfbc6823c33a67f5301d95b016abc63976
Bryan-C-W <jabrway96@gmail.com>
2018-06-21 20:01:08 -0500

    typo fix

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,7 @@
 fetch('http://www.sharproot.com/wp-json/wp/v2/posts?categories=28&_embed') 
 .then((resp) => resp.json())
 .then(function(vendors) {
-    var sipnner = document.getElementById("spinner");
+    var spinner = document.getElementById("spinner");
     spinner.style.display="none";
     acceptVendors(vendors);
 })
