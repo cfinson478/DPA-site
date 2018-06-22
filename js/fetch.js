b40b370a3ef10b30a8744929ad5af74324ae3341
Bryan-C-W <jabrway96@gmail.com>
2018-06-21 20:04:13 -0500

    master added

--- js/fetch.js
+++ js/fetch.js
@@ -1,9 +1,11 @@
+var vendors_master
 fetch('http://www.sharproot.com/wp-json/wp/v2/posts?categories=28&_embed') 
 .then((resp) => resp.json())
 .then(function(vendors) {
     var spinner = document.getElementById("spinner");
     spinner.style.display="none";
     acceptVendors(vendors);
+    vendors_master = vendors;
 })
 .catch(function() {
 console.log("Husten, we have a problem.")
