351961b15237546d2f144e36ab4efc9ef09e120d
cfinson478 <cfinson478@gmail.com>
2018-06-21 20:35:30 -0500

    cleared row when rendering vendors

--- js/vendors.js
+++ js/vendors.js
@@ -1,6 +1,7 @@
 var row = document.getElementById("vendor-row");
 
 function acceptVendors(vendors) {
+    row.innerHTML="";
     for (var i = 0; i < vendors.length; i++) {
         var vendor = `
             <div class="col-md-4 pb-1 pb-md-0 ">
@@ -8,7 +9,7 @@ function acceptVendors(vendors) {
                     <img class="card-img-top" src="${vendors[i]._embedded["wp:featuredmedia"]["0"].source_url}" alt="Card image cap">
                     <div class="card-body">
                         <h3 class="card-title">${vendors[i].title.rendered}</h3>
-                        <p>${vendors[i].excerpt}</p>
+                        <p>${vendors[i].excerpt.rendered}</p>
                     </div>
                 </div>
             </div>
