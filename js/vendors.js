8d2df8d4f09cd5cbaaa78238d383a98f2e81e83a
cfinson478 <cfinson478@gmail.com>
2018-06-21 19:41:05 -0500

    fix img src in vendors.js

--- js/vendors.js
+++ js/vendors.js
@@ -2,10 +2,11 @@ var row = document.getElementById("vendor-row");
 
 function acceptVendors(vendors) {
     for (var i = 0; i < vendors.length; i++) {
+        console.log(vendors[i]);
         var vendor = `
             <div class="col-md-4 pb-1 pb-md-0 ">
                 <div class="card">
-                    <img class="card-img-top" src="${vendors[i.featured_media]}" alt="Card image cap">
+                    <img class="card-img-top" src="${vendors[i]._embedded["wp:featuredmedia"]["0"].source_url}" alt="Card image cap">
                     <div class="card-body">
                         <h3 class="card-title">${vendors[i].title.rendered}</h3>
                         <p>${vendors[i].excerpt}</p>
@@ -17,4 +18,5 @@ function acceptVendors(vendors) {
 
     }
 }
+
 
