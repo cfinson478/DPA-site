9708d97f72edea910dcbbe73220b3cd54576382a
Bryan-C-W <jabrway96@gmail.com>
2018-06-21 19:07:57 -0500

    Data spefify
    added json specification

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,5 @@
 fetch('http://www.sharproot.com/wp-json/wp/v2/posts?categories=28') 
+.then((resp) => resp.json())
 .then(function(vendors) {
     var sipnner = document.getElementById("spinner");
     spinner.style.display="none";
